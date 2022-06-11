681588c9bddf825a7b1263804bee78965ed27a3f
SerchPro <proyects_serch@hotmail.com>
2022-06-11 11:09:10 -0500

    add useState and Buttons

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Text, View, Button } from 'react-native';
 
 export const CounterScreen = () => {
+    const [counter, setCounter] = useState(10)
   return (
     <View style={{
         flex: 1,
@@ -14,8 +15,21 @@ export const CounterScreen = () => {
                 top:-15
             }}
             >
-                Counter xxx
+                Counter {counter}
         </Text>
+
+        <Button
+            onPress={ () => setCounter( counter +1 )}
+            title="increase"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+        />
+
+<Button
+            onPress={ () => setCounter( counter +1 )}
+            title="increase"
+            accessibilityLabel="Learn more about this purple button"
+        />
     </View>
   )
 }
