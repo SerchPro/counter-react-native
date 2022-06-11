607fdabc3203a66b6884f709b736872aaf57b2d2
SerchPro <proyects_serch@hotmail.com>
2022-06-11 12:34:02 -0500

    add floating button

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,35 +1,74 @@
 import React, { useState } from 'react'
-import { Text, View, Button } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
 export const CounterScreen = () => {
     const [counter, setCounter] = useState(10)
   return (
-    <View style={{
-        flex: 1,
-        justifyContent: 'center'
-    }}>
+    <View style={styles.container}>
         <Text
-            style={{
-                textAlign:'center',
-                fontSize:40,
-                top:-15
-            }}
+            style={styles.title}
             >
                 Counter {counter}
         </Text>
 
-        <Button
+        <TouchableOpacity
+            style={ styles.fabLocationBR}
             onPress={ () => setCounter( counter +1 )}
-            title="increase"
-            color="#841584"
-            accessibilityLabel="Learn more about this purple button"
-        />
+            >
 
-<Button
-            onPress={ () => setCounter( counter +1 )}
-            title="increase"
-            accessibilityLabel="Learn more about this purple button"
-        />
+            <View style= { styles.fab}>
+                <Text style= { styles.fabText } >+1</Text>
+            </View>
+            
+        </TouchableOpacity>
+
+        <TouchableOpacity
+            style={ styles.fabLocationLT}
+            onPress={ () => setCounter( counter -1 )}
+            >
+
+            <View style= { styles.fab}>
+                <Text style= { styles.fabText } >-1</Text>
+            </View>
+            
+        </TouchableOpacity>
     </View>
   )
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+    },
+    title:{
+        textAlign:'center',
+        fontSize:40,
+        top:-15
+    },
+    fab:{
+        backgroundColor: '#5856D6',
+        width: 60,
+        height:60,
+        borderRadius: 100,
+        justifyContent: 'center'
+    },
+    fabText:{
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf: 'center'
+    },
+    fabLocationBR:{
+        position: 'absolute',
+        bottom: 25,
+        right:25
+    },
+    fabLocationLT:{
+        position: 'absolute',
+        bottom: 25,
+        left:25
+    }
+  });
